7aaf06d2b3a9ccea8d79fa06a4e988d71f946874
nandan-max <jhanandan6207@gmail.com>
2025-01-24 13:34:27 +0000

    changed config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPasword = "12345"
+accountcity = "jaipur"
+
+// accountId = 2// not allowed
+
+accountEmail = "nandan@gmail.com"
+accoutcity = "bihar"
+let accountState; //if u dont assign value javascript will show undefined
+console.log(accountId);
+
+console.log(accountEmail);
+
+console.table([accountId,accountPasword,accountEmail,accountcity,accountState]);
+
+/* 
+prefer not to use var because of issue in block scope and functional use */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //it treats all js code as newer version//
+// alert(3+3) //we are using node js it will not work here ,it will work if write in  devloper console of browser or in html//
+
+ //console.log(3+3);console.log("hitesh") //this is not good way to write code just wrte code which readeblity high 
+//console.log(3+3);
+//console.log("hitesh")//
+
+let name ="hitesh"
+let age = 18
+let isloggedIn = false
+
+
+
+// number => 2 to the power 53
+// bigint => it is used for big number speciall trading, stock market etc
+// string => ""
+// boolean => trur/false
+// null => it is stand alone value / null is representaion of empty value
+// undefined => it means that value is not assigned or value is not defined
+// symbol => symbol is used for uniqueness
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof undefined); //iska output undefined hoga
+console.log(typeof null) //null is object data type//
