b80a4bcafbe9f149a86e90d0822a3c1b8c62ef18
nandan-max <jhanandan6207@gmail.com>
2025-01-24 16:00:36 +0000

    config changes done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "hitesh"
+
+console.log(typeof score);
+console.log(typeof (score));     
+
+let valueInNumber = Number(score) //Number is datatype use N in capital//
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // it will give output NaN <means not a number>
+
+//notes//
+//if we put let score = downward written  types value then output are given //
+//"33"=> 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
